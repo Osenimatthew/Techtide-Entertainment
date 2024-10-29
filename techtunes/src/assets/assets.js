453a9d36a66e0a68bad0a5c1572276e544e90187
Osenimatthew <osenimatthew9@gmail.com>
2024-10-29 01:50:41 -0700

    core web app files, including main components, styles, and asset images.

--- techtunes/src/assets/assets.js
+++ techtunes/src/assets/assets.js
@@ -1,11 +1,11 @@
 import playlistImage from "./playlist.webp";
-import logo from "/logo.jpg";
-import background1 from "/musicback.jpg";
-import background2 from "/musicback2.jpeg";
-import background3 from "/musicback2.webp";
-import background4 from "/musicback3.jpg";
-import background5 from "/musicback4.jpg";
-import musiclogo from "/musiclogo.jpg";
+import logo from "./logo.jpg";
+import background1 from "./musicback.jpg";
+import background2 from "./musicback2.jpeg";
+import background3 from "./musicback2.webp";
+import background4 from "./musicback3.jpg";
+import background5 from "./musicback4.jpg";
+import musiclogo from "./musiclogo.jpg";
 
 export {
   playlistImage,
